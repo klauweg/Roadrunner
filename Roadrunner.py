8c648096f1d85430d283aa8bb11f8960dc39198e
Klaus W <vortex25@gmx.de>
2019-07-25 11:43:02 +0200

    Wir ersetzen den alten "boot" spieler durch einen neuen Character

--- Roadrunner.py
+++ Roadrunner.py
@@ -3,7 +3,7 @@ import pygame
 import pytmx
 from pytmx.util_pygame import load_pygame
 
-    
+
 # initialize pygame
 pygame.init()
 
@@ -11,15 +11,39 @@ pygame.init()
 game_display = pygame.display.set_mode((640, 640))
 pytmx_map = load_pygame("Fertigtest.tmx") 
 
-
+# invent a moveable Game Character:
 class Character(pygame.sprite.Sprite):
-    def __init__(self, image, x, y):
-       pygame.sprite.Sprite.__init__(self)        # Call the parent class (Sprite) constructor
-       self.image = image
-       self.rect = self.image.get_rect()
-       self.rect.topleft = (x,y)
+    def __init__(self, image, x, y, speedx, speedy):
+        pygame.sprite.Sprite.__init__(self)        # Call the parent class (Sprite) constructor
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.speedx = speedx
+        self.speedy = speedy
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.oldx = self.x
+        self.oldy = self.y
+    def update(self):
+        if self.x !=0 or self.y != 0:
+            self.oldx = self.x
+            self.oldy = self.y
+            self.x = self.x + self.speedx
+            self.y = self.y + self.speedy
+            self.rect.x = int(self.x)
+            self.rect.y = int(self.y)
+    def moveby(self, x, y):
+        self.oldx = self.x
+        self.oldy = self.y
+        self.x = self.x + x
+        self.y = self.y + y
+    def undo(self):
+        self.x = self.oldx
+        self.y = self.oldy
+    def draw(self, surface):
+        surface.blit( self.image, self.rect )
 
-# create background layer
+# create background surface
 background_surf = pygame.Surface((20*32, 20*32))
 for x, y, gid in pytmx_map.get_layer_by_name("Kachelebene"):
     image = pytmx_map.get_tile_image_by_gid( gid ) # Hier wird die tmx gid verwendet um die Grafik zu bekommen
@@ -30,16 +54,22 @@ spritegroups={}
 for objectgroup in pytmx_map.objectgroups:
     spritegroups[objectgroup.name]=pygame.sprite.Group()
     for object in objectgroup:
-        spritegroups[objectgroup.name].add( Character( object.image, object.x, object.y ) )
+        spritegroups[objectgroup.name].add( Character( object.image, object.x, object.y, 0, 0 ) )
 
 
+spritegroups['npc'] = pygame.sprite.Group()
+player_surf=pygame.Surface( (16,16) )
+player_surf.fill( pygame.Color( 164,0,0 ) )
+spritegroups['npc'].add( Character( player_surf, 75, 10, 0.5, 0.5 ) )
+spritegroups['npc'].add( Character( player_surf, 400, 30, 0.2, 0.1 ) )
+spritegroups['npc'].add( Character( player_surf, 1, 30, 0.4, 0.3 ) )
 
-# create sprite
-boot = pygame.Surface( (16,16) )
-boot.fill( pygame.Color(153,0,0) )
-bootpos = boot.get_rect()
-bootpos.x = 475
-bootpos.y = 30
+spritegroups['player'] = pygame.sprite.Group()
+player_surf=pygame.Surface( (16,16) )
+player_surf.fill( pygame.Color( 0,164,200 ) )
+player = Character( player_surf, 400, 400, 0, 0 )
+spritegroups['player'].add( player )
+
 
 
 loop = True
@@ -61,8 +91,6 @@ def textwrite(tx, ty,message,color,font):
 
 
 while(loop):
-    oldbootpos = bootpos.copy() # Merken falls Kollision stattfindet
-    
     # Bearbeiten der Message Queue:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,13 +109,14 @@ while(loop):
         pygame.quit()
         quit()
     elif lastkey == pygame.K_LEFT:
-        bootpos.x += -2
+        player.moveby( -2, 0 )
     elif lastkey == pygame.K_RIGHT:
-        bootpos.x += 2
+        player.moveby( 2, 0 )
     elif lastkey == pygame.K_DOWN:
-        bootpos.y += 2
+        player.moveby( 0, 2 )
     elif lastkey == pygame.K_UP:
-        bootpos.y += -2
+        player.moveby( 0, -2 )
+
 
         
                 
@@ -112,9 +141,11 @@ while(loop):
     game_display.blit(background_surf, (0,0)) # Hintergrund aufs Gamedisplay kopieren
     
     for spritegroup in spritegroups.values():
+        spritegroup.update()
+        
+    for spritegroup in spritegroups.values():
         spritegroup.draw( game_display )
     
-    game_display.blit(boot, bootpos)     # Das Boot ins Gamedisplay kopieren
     
 #    textwrite( bootpos.x+16, bootpos.y-32,playermessage, (0, 0, 0) , fonts)
     
